b6cac14accb4af1f11378164e9f66c9ff22591d9
Łukasz Sławomir Szopa <l.szopa@stud.elka.pw.edu.pl>
2022-10-01 12:42:50 +0200

    D_initial commit

---
+++ app/src/main/java/com/example/healthapp/AboutActivity.java
@@ -0,0 +1,44 @@
+package com.example.healthapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.MenuItem;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("O aplikacji");
+        }
+
+    }
+
+    public void goToGitAccount(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/lukkiszo"));
+        startActivity(browserIntent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
